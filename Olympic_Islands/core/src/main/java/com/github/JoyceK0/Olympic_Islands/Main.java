a3bdaea5b2ea54c60a21dd0a3b0d3eda223ad747
Kavya V <kavyavishnu09@gmail.com>
2026-05-31 19:25:11 -0400

    Video #3 till timestamp 37:33 Parts done: ApplicationListener and Game Implementing the screen cache Spritebatch Viewport and Camera AssetService* *note for the map asset the code is all just comments since we don't have the tile yet

--- Olympic_Islands/core/src/main/java/com/github/JoyceK0/Olympic_Islands/Main.java
+++ Olympic_Islands/core/src/main/java/com/github/JoyceK0/Olympic_Islands/Main.java
@@ -1,13 +1,66 @@
 package com.github.JoyceK0.Olympic_Islands;
 
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.assets.loaders.resolvers.InternalFileHandleResolver;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.utils.GdxRuntimeException;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.github.JoyceK0.Olympic_Islands.asset.AssetService;
 
+import java.util.HashMap;
+import java.util.Map;
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends Game {
+    public static  final float WORLD_WIDTH = 16f;
+    public static final float WORLD_HEIGHT = 9f;
+    private Batch batch;
+    private OrthographicCamera camera;
+    private Viewport viewport;
+    private AssetService assetService;
+
+    private final Map<Class<? extends Screen>, Screen> screenCache = new HashMap<>();
+
     @Override
     public void create() {
+        this.batch = new SpriteBatch();
+        this.camera = new OrthographicCamera();
+        this.viewport = new FitViewport(WORLD_WIDTH, WORLD_HEIGHT, camera);
+        this.assetService = new AssetService(new InternalFileHandleResolver());
 
-        setScreen(new FirstScreen());
+        addScreen(new GameScreen(this));
+        setScreen(GameScreen.class);
 
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width,height,true);
+        super.resize(width, height);
+    }
+
+    public void addScreen(Screen screen){
+        screenCache.put(screen.getClass(),screen);
+    }
+
+    public void setScreen(Class<? extends Screen>screenClass){
+        Screen screen = screenCache.get(screenClass);
+        if(screen == null){
+            throw new GdxRuntimeException("No screen with this class " + screenClass +" found");
+        }
+        super.setScreen(screen);
+    }
+
+    @Override
+    public void dispose() {
+        screenCache.values().forEach(Screen::dispose);
+        screenCache.clear();
+
+        this.batch.dispose();
+        this.assetService.debugDiagnostics();
+        this.assetService.dispose();
     }
 }
---
+++ Olympic_Islands/core/src/main/java/com/github/JoyceK0/Olympic_Islands/asset/MapAsset.java
@@ -0,0 +1,24 @@
+package com.github.JoyceK0.Olympic_Islands.asset;
+
+import com.badlogic.gdx.assets.AssetDescriptor;
+import com.badlogic.gdx.maps.tiled.BaseTiledMapLoader;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.github.JoyceK0.Olympic_Islands.Main;
+//tile map to be added
+//public enum MapAsset implements Asset<TiledMap> {
+    //MAIN("*main.tmx* ") //check if file is called that
+
+    //private final AssetDescriptor<TiledMap> descriptor;
+
+     //MapAsset(String mapName){
+         //BaseTiledMapLoader.Parameters parameters = new BaseTiledMapLoader.Parameters();
+        // parameters.projectFilePath = "maps/tiled.tiled-project"; //check file naem
+      //  this.descriptor = new AssetDescriptor<>("maps/"+mapName, TiledMap.class, parameters); //check if folder is called that
+    //}
+
+   // @Override
+    //public AssetDescriptor<TiledMap> getDescriptor() {
+     //   return this.descriptor;
+    //}
+//}

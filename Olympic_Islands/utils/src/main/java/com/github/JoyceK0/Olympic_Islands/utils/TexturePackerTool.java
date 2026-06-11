e6c77a2c7a9ca92ed5194983ef33fa0c1e6b3ccd
Jigna <jignavpathak@gmail.com>
2026-06-11 08:38:55 -0400

    9:15 Atlas Video, Created utils folder and added sub-directories.

---
+++ Olympic_Islands/utils/src/main/java/com/github/JoyceK0/Olympic_Islands/utils/TexturePackerTool.java
@@ -0,0 +1,17 @@
+package com.github.JoyceK0.Olympic_Islands.utils;
+
+import com.badlogic.gdx.tools.texturepacker.TexturePacker;
+
+public class TexturePackerTool {
+
+    public static void main(String[] args) {
+
+        String inputDir = "";
+        String outputDir = "";
+        String packFileName = "";
+
+        TexturePacker.process(inputDir, outputDir, packFileName);
+
+    }
+
+}

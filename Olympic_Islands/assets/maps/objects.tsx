02a12c83ec3179b029aaddd8c6a1031226181fe2
Hi <joycexyke@gmail.com>
2026-06-11 19:33:04 -0400

    Finished state after all videos.

---
+++ Olympic_Islands/assets/maps/objects.tsx
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="objects" tilewidth="96" tileheight="128" tilecount="3" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <properties>
+  <property name="atlasAsset" value="OBJECTS"/>
+ </properties>
+ <tile id="5">
+  <properties>
+   <property name="atlastAsset" value="OBJECTS"/>
+  </properties>
+  <image source="objects/lucky.png" width="21" height="21"/>
+ </tile>
+ <tile id="6">
+  <properties>
+   <property name="atlasAsset" value="OBJECTS"/>
+  </properties>
+  <image source="objects/tree.png" width="64" height="80"/>
+ </tile>
+ <tile id="7">
+  <properties>
+   <property name="atlasAsset" value="OBJECTS"/>
+  </properties>
+  <image source="objects/houseBig.png" width="96" height="128"/>
+ </tile>
+</tileset>

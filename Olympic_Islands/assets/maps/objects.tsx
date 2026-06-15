93e3a567dcee074a7f490f28db1e950e1c5d6eda
Hi <joycexyke@gmail.com>
2026-06-14 22:24:24 -0400

    Finished tiled map, slight fix to Lucky's idle animation.

--- Olympic_Islands/assets/maps/objects.tsx
+++ Olympic_Islands/assets/maps/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="objects" tilewidth="96" tileheight="128" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.12.1" name="objects" tilewidth="74" tileheight="113" tilecount="4" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <properties>
   <property name="atlasAsset" value="OBJECTS"/>
@@ -18,22 +18,35 @@
    </object>
   </objectgroup>
  </tile>
- <tile id="6" type="Prop">
-  <properties>
-   <property name="atlasAsset" value="OBJECTS"/>
-  </properties>
-  <image source="objects/tree.png" width="64" height="80"/>
- </tile>
  <tile id="7" type="Prop">
   <properties>
    <property name="atlasAsset" value="OBJECTS"/>
   </properties>
-  <image source="objects/houseBig.png" width="96" height="128"/>
+  <image source="objects/houseBig.png" width="74" height="113"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="4.31899" y="84.2093" width="64.9706" height="31.3701"/>
+  </objectgroup>
  </tile>
  <tile id="8" type="Prop">
   <properties>
    <property name="atlasAsset" value="OBJECTS"/>
   </properties>
   <image source="objects/horse.png" width="60" height="33"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="14.785" y="33.2335">
+    <polygon points="0,0 26.2989,0.13084 33.3643,-6.28034 33.8877,-11.514 0.654202,-10.8598"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="9" type="Prop">
+  <properties>
+   <property name="atlasAsset" value="OBJECTS"/>
+  </properties>
+  <image source="objects/tree.png" width="42" height="63"/>
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="12.7066" y="55.1714">
+    <polygon points="0,0 4.10213,-6.48887 4.10213,-10.2282 13.8072,-10.4482 13.9072,-7.25874 19.4101,-1.75969 19.31,2.19962 19.21,3.95931 0.300156,3.84933"/>
+   </object>
+  </objectgroup>
  </tile>
 </tileset>

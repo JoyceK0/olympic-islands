a2aa58f7d47242a09a1fd9bdb659cb290f25d0eb
Hi <joycexyke@gmail.com>
2026-06-14 22:57:10 -0400

    Added Stolk.

--- Olympic_Islands/assets/maps/objects.tsx
+++ Olympic_Islands/assets/maps/objects.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.1" name="objects" tilewidth="74" tileheight="113" tilecount="4" columns="0">
+<tileset version="1.10" tiledversion="1.12.1" name="objects" tilewidth="74" tileheight="113" tilecount="5" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <properties>
   <property name="atlasAsset" value="OBJECTS"/>
@@ -47,6 +47,15 @@
    <object id="2" x="12.7066" y="55.1714">
     <polygon points="0,0 4.10213,-6.48887 4.10213,-10.2282 13.8072,-10.4482 13.9072,-7.25874 19.4101,-1.75969 19.31,2.19962 19.21,3.95931 0.300156,3.84933"/>
    </object>
+  </objectgroup>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="atlasAsset" value="OBJECTS"/>
+  </properties>
+  <image source="objects/stolk.png" width="15" height="34"/>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1.64408" y="24.2744" width="11.702" height="9.7678"/>
   </objectgroup>
  </tile>
 </tileset>
